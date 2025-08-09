230ab18facfca3119257eadef750cc6bb3000ee5
Abhishek <abhishekborole@gmail.com>
2025-08-09 10:24:35 +0530

    Fixed

---
+++ Scripts/Create_OpenShift_VMs.ps1
@@ -0,0 +1,32 @@
+﻿$VMHost = Get-VMHost
+$DataStore = Get-Datastore -Name "Datastore-SSD-01"
+$VMs = "Master1",
+"Master2",
+"Master3",
+"Worker1",
+"Worker2",
+"Bootstrap"
+
+
+$VMs | where {
+$vmName = $_
+#$vm = New-VM -Name $vmName `
+#  -VMHost $VMHost `
+#  -DiskGB 120 `
+#  -DiskStorageFormat Thin `
+#  -MemoryGB 16 `
+#  -NumCpu 4 `
+#  -Datastore $DataStore `
+#  -NetworkName "Nested - Trunked" `
+#  -GuestId "other3xLinux64Guest"
+#
+## Set the SCSI controller to ParaVirtual
+#Get-ScsiController -VM $vm | Set-ScsiController -Type ParaVirtual
+#
+## Add CD/DVD drive and mount the ISO
+#New-CDDrive -VM $vm -IsoPath "[Datastore-SSD-01] ISO/rhcos-4.19.0-x86_64-live-iso.x86_64.iso" -StartConnected
+                               
+
+get-vm $vmName |Start-VM
+
+}
---
+++ Scripts/Create_VM_Snapshot_From_ESXi.ps1
@@ -0,0 +1,18 @@
+﻿#"" | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString | Out-File "D:\Abhishek\Lab\ESXi-Psw.txt"
+
+$User = "root"
+$File = "D:\Abhishek\Lab\ESXi-Psw.txt"
+$MyCredential=New-Object -TypeName System.Management.Automation.PSCredential `
+ -ArgumentList $User, (Get-Content $File | ConvertTo-SecureString)
+
+$connection = Connect-VIServer -Server 192.168.1.51 -Credential $MyCredential -Force
+$VMHost = Get-VMHost
+$vmNames = "GB20250161","GB20250162","GB20250163","GB20250164","GB20250165","GB20250166","GB20250167";
+
+foreach($vmName in $vmNames)
+{
+
+    $vm = $VMHost | get-vm -name $vmName;
+
+    $vm | New-Snapshot -Name "Initial Snapshot" -Confirm:$false
+}
